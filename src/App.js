c58827131f42c65dc416d7f36cfda7fa84511d5a
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-05-19 18:00:50 +0100

    Integrated weather API

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         <div className="weather-app-wrapper">
           <div className="weather-app">
             <Form />
-            <Current />
+            <Current defaultCity="Matosinhos"/>
             <Forecast />
           </div>
         </div>
--- src/Current.js
+++ src/Current.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import "./Current.css";
 
-export default function Current() {
+export default function Current(props) {
+  const [currentData, setCurrentData] = useState({ready:false});
+  function handleResponse (response) {
+    setCurrentData({
+      ready:true,
+      city:response.data.name,
+      date:"Friday, 23 Apr 15:25",
+      temperature: response.data.main.temp,
+      description:response.data.weather[0].description,
+      wind:response.data.wind.speed,
+      humidity:response.data.main.humidity,
+      feelsLike:response.data.main.feels_like,
+      iconUrl:`http://openweathermap.org/img/wn/10d@2x.png`
+      
+    });
+  }
+
+  if (currentData.ready) {
     return (
       <div>
-        <h1 id="city">MATOSINHOS</h1>
-        <h2 id="date">Friday, 23 Apr 15:25</h2>
+        <h1 id="city">{currentData.city.toUpperCase()}</h1>
+        <h2 id="date">{currentData.date}</h2>
         <div className="row">
           <div className="col-4">
             <ul>
-              <li id="description">Moderate Rain</li>
+              <li id="description">{currentData.description}</li>
               <li>
                 <img
-                  src="http://openweathermap.org/img/wn/10d@2x.png"
-                  alt=""
+                  src={currentData.iconUrl}
+                  alt={currentData.description}
                   id="icon"
                   className="weather-icon"
                 />
@@ -21,20 +39,20 @@ export default function Current() {
             </ul>
           </div>
           <div className="col-4">
-            <strong id="temperature">20</strong>
+            <strong id="temperature">{Math.round(currentData.temperature)}</strong>
             <span className="units"> ºC|ºF</span>
           </div>
           <div className="col-4">
             <div className="specifications">
               <ul>
                 <li>
-                  Feels like: <span id="realfeel">20</span>º
+                  Feels like: <span id="realfeel">{Math.round(currentData.feelsLike)}</span>º
                 </li>
                 <li>
-                  Wind: <span id="wind">5</span> m/s
+                  Wind: <span id="wind">{Math.round(currentData.wind)}</span> m/s
                 </li>
                 <li>
-                  Humidity: <span id="humidity">72</span>%
+                  Humidity: <span id="humidity">{Math.round(currentData.humidity)}</span>%
                 </li>
               </ul>
             </div>
@@ -42,5 +60,16 @@ export default function Current() {
         </div>
       </div>
     );
+
+  } else {
+    const apiKey = "f2307cbce532cfdeb3168c7d625e3421"
+    let units ="metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&units=${units}&appid=${apiKey}`;
+  
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
   }
+  
+}
   
