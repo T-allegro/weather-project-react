ecf9d01d7cb72615e9c28fc0e56ceac58db26704
T-allegro <78311737+T-allegro@users.noreply.github.com>
2021-05-29 09:31:06 +0100

    Corrected code

--- src/CurrentInfo.js
+++ src/CurrentInfo.js
@@ -6,19 +6,18 @@ import "./CurrentInfo.css";
 export default function CurrentInfo (props) {
     return (
         <div>
-        <h1 id="city">{props.data.city.toUpperCase()}</h1>
-        <h2 id="date">
+        <h1 className="city">{props.data.city.toUpperCase()}</h1>
+        <h2 className="date">
           <FormattedDate date={props.data.date} />
         </h2>
         <div className="row">
           <div className="col-4">
             <ul>
-              <li id="description">{props.data.description}</li>
+              <li className="description">{props.data.description}</li>
               <li>
                 <img
                   src={props.data.iconUrl}
                   alt={props.data.description}
-                  id="icon"
                   className="weather-icon"
                 />
               </li>
@@ -31,13 +30,13 @@ export default function CurrentInfo (props) {
             <div className="specifications">
               <ul>
                 <li>
-                  Feels like: <span id="realfeel">{Math.round(props.data.feelsLike)}</span>º
+                  Feels like: <span>{Math.round(props.data.feelsLike)}</span>º
                 </li>
                 <li>
-                  Wind: <span id="wind">{Math.round(props.data.wind)}</span> m/s
+                  Wind: <span>{Math.round(props.data.wind)}</span> m/s
                 </li>
                 <li>
-                  Humidity: <span id="humidity">{Math.round(props.data.humidity)}</span>%
+                  Humidity: <span>{Math.round(props.data.humidity)}</span>%
                 </li>
               </ul>
             </div>
